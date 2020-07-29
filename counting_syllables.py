f470927218482e934461d2bc95feae2f3ac135db
Alex Hill <alhillme@gmail.com>
2020-07-28 18:53:33 -0600

    finished counting_syllables.py

--- counting_syllables.py
+++ counting_syllables.py
@@ -2,10 +2,8 @@
 # https://pythonprinciples.com/challenges/Counting-syllables/ #
 
 def count(str_in):
-    syllables = 0
+    syllables = 1
     for i in range(len(str_in)):
-        if('a' in str_in[i] or 'e' in str_in[i] or 'i' in str_in[i] or 'o' in str_in[i] or 'u' in str_in[i]):
+        if(str_in[i] == '-'):
             syllables += 1
-        else:
-            pass
     return syllables
