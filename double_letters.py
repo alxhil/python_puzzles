d9fc3a1b742b49f1fdb894ae4dc2cec26a3a4016
Alex Hill <alhillme@gmail.com>
2020-07-28 18:33:05 -0600

    finished double_letters.py

---
+++ double_letters.py
@@ -0,0 +1,12 @@
+# python challenge 6 #
+# https://pythonprinciples.com/challenges/Double-letters/ #
+
+def double_letters(str_in):
+    temp_str = ""
+    for i in range(len(str_in)):
+        if(temp_str == str_in[i]):
+            return True
+        else:
+            temp_str = str_in[i]
+    return False
+
