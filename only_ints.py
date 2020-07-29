c1fbc93dc1aaa5d9e06b0961b83f1cb3c0d17318
Alex Hill <alhillme@gmail.com>
2020-07-28 18:30:08 -0600

    finished only_ints and random.py

---
+++ only_ints.py
@@ -0,0 +1,8 @@
+#python challenge 5 #
+# https://pythonprinciples.com/challenges/Type-check/ #
+
+def only_ints(in1, in2):
+    if(type(in1) == int and type(in2) == int):
+        return True
+    else:
+        return False
