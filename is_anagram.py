26b57f6b269f25924db8164502971bef8f7addcc
Alex Hill <alhillme@gmail.com>
2020-07-29 08:55:34 -0600

    Finished is_anagram and added name to all files

---
+++ is_anagram.py
@@ -0,0 +1,15 @@
+def is_anagram(in_1, in_2):
+    if(len(in_1) != len(in_2)):
+        return False
+    for i in range(len(in_1)):
+        if(in_1[i] in in_2):
+            if(i == len(in_1)-1):
+                return True
+            else:
+                in_2 = in_2.replace(in_1[i], '0', 1)
+                in_1 = in_1.replace(in_1[i], '0', 1)
+        else:
+            return False
+
+
+print(is_anagram('test', 'tess'))
--- random_puzzle.py
+++ random_puzzle.py
@@ -1,3 +1,6 @@
+# Python Challenge 4 #
+# Alex Hill #
+
 import random
 def random_number():
     return random.randint(0,100)
